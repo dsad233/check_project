4e0f7e65eed6255c0e6c1dd3f41d28de22a166b1
devdotcor <ksl1421@mement.ai>
2024-10-10 20:30:28 +0900

    Delete app/api directory

--- app/api/routes/users/schema/usersSchema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-# 유저 수정 스키마
-class UsersEdit(BaseModel):
-    password : str
-    nickname : str
-    isOpen : Optional[bool] = None
-    image : Optional[str] = None
