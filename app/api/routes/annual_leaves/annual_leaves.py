b95bb13c32a11945dc470a4baeb3e7e348357ed4
kudongku <kudongku@naver.com>
2024-10-10 22:06:13 +0900

    Feat : 연차 신청, 신청 기록 조회 기능 구현

---
+++ app/api/routes/annual_leaves/annual_leaves.py
@@ -0,0 +1,42 @@
+from app.api.core.database import Session
+from fastapi import APIRouter, Depends
+from app.api.middleware.tokenVerify import vaildate_Token
+from app.api.routes.annual_leaves.schema.annual_leave_schema import AnnualLeaveCreate
+from app.api.models.models import AnnualLeave, Users
+from app.api.routes.annual_leaves.schema.annual_leave_schema import AnnualLeaveStatus
+
+router = APIRouter(dependencies=[Depends(vaildate_Token)])
+annual_leaves = Session()
+
+@router.get('')
+async def getAnnualLeave(
+    current_user: Users = Depends(vaildate_Token)
+):
+    try:
+        getAnnualLeave = annual_leaves.query(AnnualLeave).filter(AnnualLeave.proposer_id == current_user.id).all()
+        return { "message" : "연차 조회에 성공하였습니다.", "data" : getAnnualLeave }
+    except Exception as err:
+        print("에러가 발생하였습니다.")
+        print(err)
+
+@router.post('')
+async def createAnnualLeave(
+    annualLeaveCreate: AnnualLeaveCreate,
+    current_user: Users = Depends(vaildate_Token)
+):
+    try:
+        create = AnnualLeave(
+            type = annualLeaveCreate.type,
+            manager_id = annualLeaveCreate.manager_id,
+            proposer_id = current_user.id,
+            date_count = annualLeaveCreate.date_count,
+            application_date = annualLeaveCreate.application_date,
+            proposer_note = annualLeaveCreate.proposer_note,
+            manager_note = annualLeaveCreate.manager_note,
+        )
+        annual_leaves.add(create)
+        annual_leaves.commit()
+        return { "message" : "연차 생성에 성공하였습니다." }    
+    except Exception as err:
+        print("에러가 발생하였습니다.")
+        print(err)
