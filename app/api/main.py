989047b1791fa9ef2268abf4213547a8fc766020
devdotcor <ksl1421@mement.ai>
2024-10-11 14:07:26 +0900

    Fix : Git 오류 수정

--- app/api/main.py
+++ app/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.api.routes.auth import auth
+from app.api.routes.overtimes import overtimes
 
 app = APIRouter()
 
 app.include_router(auth.router, prefix='/auth', tags=['Auth'])
+app.include_router(overtimes.router, prefix='/overtime', tags=['Overtime'])
