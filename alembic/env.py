c0ffd51f4df805665d9d2ddea129af10e2f3e9e4
bboodd <gkrgus9@gmail.com>
2024-10-11 22:47:41 +0900

    fix: 환경설정 분리

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,13 @@
+import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+from sqlalchemy.engine import Connection
+
 from alembic import context
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,6 +26,9 @@ from app.models.models import Base
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -54,6 +61,27 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
+    and associate a connection with the context."""
+
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
@@ -62,21 +90,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    configuration = config.get_section(config.config_ini_section)
-    configuration['sqlalchemy.url'] = get_url()
-    connectable = engine_from_config(
-        configuration,
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,51 @@
+from pydantic_settings import BaseSettings
+from typing import Union
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+class DevSettings(BaseSettings):
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
 
-class Settings:
-    JWT_SECRET_KEY : str = os.getenv('JWT_SECRET_KEY')
-    JWT_ALGORITHM : str = os.getenv('JWT_ALGORITHM')
+    MYSQL_USER: str
+    MYSQL_PASSWORD: str
+    MYSQL_HOST: str
+    MYSQL_PORT: int
+    MYSQL_DATABASE: str
+    MYSQL_ROOT_PASSWORD: str
 
-    DATABASE_URL : str = os.getenv('DATABASE_URL')
+    @property
+    def DATABASE_URL(self) -> str:
+        return f'mysql+aiomysql://{self.MYSQL_USER}:{self.MYSQL_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}'
+    
+    class Config:
+        env_file = ('.env', '.env.dev')
 
-settings = Settings()
+class ProdSettings(BaseSettings):
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+
+    RDS_USER: str
+    RDS_PASSWORD: str
+    RDS_HOST: str
+    RDS_PORT: int
+    RDS_DATABASE: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f'mysql+aiomysql://{self.RDS_USER}:{self.RDS_PASSWORD}@{self.RDS_HOST}:{self.RDS_PORT}/{self.RDS_DATABASE}'
+    
+    class Config:
+        env_file = ('.env', '.env.prod')
+
+def load_settings() -> Union[DevSettings, ProdSettings]:
+    mode = os.getenv('MODE', 'dev')
+
+    if mode == 'dev':
+        return DevSettings()
+    elif mode == 'prod':
+        return ProdSettings()
+    else:
+        raise ValueError(f"Unsupported environment: {mode}")
+    
+
+settings = load_settings()
