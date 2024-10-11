30591fd1ab68670ef27959a9bff04e93865ff060
kudongku <kudongku@naver.com>
2024-10-11 11:47:35 +0900

    Refactor : response에 대한 dto 생성

--- app/api/routes/annual_leaves/annual_leaves.py
+++ app/api/routes/annual_leaves/annual_leaves.py
@@ -2,7 +2,7 @@ from app.core.database import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.middleware.tokenVerify import vaildate_Token
-from app.api.routes.annual_leaves.schema.annual_leave_schema import AnnualLeaveCreate, AnnualLeaveStatus, AnnualLeaveApprove, AnnualLeaveUpdate
+from app.api.routes.annual_leaves.schema.annual_leave_schema import AnnualLeaveCreate, AnnualLeaveStatus, AnnualLeaveApprove, AnnualLeaveUpdate, AnnualLeaveListsResponse
 from app.models.models import AnnualLeave, Users
 
 router = APIRouter(dependencies=[Depends(vaildate_Token)])
@@ -13,10 +13,27 @@ async def getAnnualLeave(
     db: Session = Depends(get_db)
 ):
     try:
-        getAnnualLeave = db.query(AnnualLeave).filter(AnnualLeave.proposer_id == current_user.id, AnnualLeave.deleted_yn == 'N').all()
-        return { "message" : "연차 조회에 성공하였습니다.", "data" : getAnnualLeave }
+        annual_leaves = db.query(AnnualLeave).filter(AnnualLeave.proposer_id == current_user.id, AnnualLeave.deleted_yn == 'N').all()
+        annual_leave_dtos=[]    
+        for leave in annual_leaves:
+            dto = AnnualLeaveListsResponse(
+                id = leave.annual_leave_id,
+                proposer_name = leave.proposer.name,
+                work_part = leave.proposer.work_part,
+                application_date = leave.application_date,
+                type = leave.type,
+                date_count = leave.date_count,
+                status = AnnualLeaveStatus(leave.application_status),
+                proposer_note = leave.proposer_note,
+                manager_note = leave.manager_note if leave.manager_note else None,
+                updated_at = leave.updated_at,
+                manager_name = leave.manager.name if leave.manager else None
+            )
+            annual_leave_dtos.append(dto)
+        return { "message" : "연차 조회에 성공하였습니다.", "data" : annual_leave_dtos }
     except Exception as err:
-        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다." + err)
+        print(err)
+        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다")
 
 @router.get('/pending')
 async def getPendingAnnualLeave(
@@ -26,10 +43,27 @@ async def getPendingAnnualLeave(
     if not current_user.is_admin:
         raise HTTPException(status_code=403, detail="권한이 없습니다.")
     try:
-        getPendingAnnualLeave = db.query(AnnualLeave).filter(AnnualLeave.status == AnnualLeaveStatus.PENDING, AnnualLeave.deleted_yn == 'N').all()
-        return { "message" : "연차 조회에 성공하였습니다.", "data" : getPendingAnnualLeave }
+        getPendingAnnualLeaves = db.query(AnnualLeave).filter(AnnualLeave.status == AnnualLeaveStatus.PENDING, AnnualLeave.deleted_yn == 'N').all()
+        annual_leave_dtos=[]    
+        for leave in getPendingAnnualLeaves:
+            dto = AnnualLeaveListsResponse(
+                id = leave.annual_leave_id,
+                proposer_name = leave.proposer.name,
+                work_part = leave.proposer.work_part,
+                application_date = leave.application_date,
+                type = leave.type,
+                date_count = leave.date_count,
+                status = AnnualLeaveStatus(leave.application_status),
+                proposer_note = leave.proposer_note,
+                manager_note = leave.manager_note if leave.manager_note else None,
+                updated_at = leave.updated_at,
+                manager_name = leave.manager.name if leave.manager else None
+            )
+            annual_leave_dtos.append(dto)
+        return { "message" : "연차 조회에 성공하였습니다.", "data" : annual_leave_dtos }
     except Exception as err:
-        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다." + err)
+        print(err)
+        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다." )
 
 @router.post('')
 async def createAnnualLeave(
@@ -50,6 +84,7 @@ async def createAnnualLeave(
         return { "message" : "연차 생성에 성공하였습니다." }    
     except Exception as err:
         db.rollback()
+        print(err)
         raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다.")
 
 @router.post('/{id}')
@@ -78,6 +113,7 @@ async def approveAnnualLeave(
         raise
     except Exception as err:
         db.rollback()
+        print(err)
         raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다.")
 
 @router.patch('/{id}')
@@ -108,7 +144,8 @@ async def updateAnnualLeave(
         raise
     except Exception as err:
         db.rollback()
-        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다." + err)
+        print(err)
+        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다.")
 
 @router.delete('/{id}')
 async def deleteAnnualLeave(
@@ -134,4 +171,5 @@ async def deleteAnnualLeave(
         raise
     except Exception as err:
         db.rollback()
-        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다." + err)
+        print(err)
+        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다.")
