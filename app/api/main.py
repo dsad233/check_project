b0134c2b35a5432b744e32a532f651af36ab0876
devdotcor <ksl1421@mement.ai>
2024-10-13 02:45:35 +0900

    Fix : Git 충돌 오류 해결

--- app/api/main.py
+++ app/api/main.py
@@ -5,7 +5,7 @@ from app.api.routes.parts import parts
 from app.api.routes.users import users
 from app.api.routes.branch_policies import branch_policies
 from app.api.routes.parts import parts
-
+from app.api.routes.users import users
 # from app.api.routes.annual_leaves import annual_leaves
 # from app.api.routes.overtimes import overtimes
 
@@ -18,4 +18,6 @@ app.include_router(
     branch_policies.router, prefix="/branch_policies", tags=["Branch_policies"]
 )
 app.include_router(parts.router, prefix="/parts", tags=["Parts"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
+# app.include_router(annual_leaves.router, prefix='/annual-leaves', tags=['Annual Leaves'])
 # app.include_router(overtimes.router, prefix='/overtime', tags=['Overtime'])
