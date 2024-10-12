945fff5f4740df754676c077a61c71f09a949471
kudongku <kudongku@naver.com>
2024-10-12 18:36:30 +0900

    Feat : main에 router 추가

--- app/api/main.py
+++ app/api/main.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter
 
 from app.api.routes.auth import auth
 from app.api.routes.parts import parts
+
 # from app.api.routes.annual_leaves import annual_leaves
 # from app.api.routes.overtimes import overtimes
 
 app = APIRouter()
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
-app.include_router(parts.router, prefix="/parts", tags=["Parts"])   
+app.include_router(parts.router, prefix="/parts", tags=["Parts"])
 # app.include_router(annual_leaves.router, prefix='/annual-leaves', tags=['Annual Leaves'])
 # app.include_router(overtimes.router, prefix='/overtime', tags=['Overtime'])
