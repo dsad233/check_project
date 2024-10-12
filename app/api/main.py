aaa2519ada75804f0a8be02d367e7f7ce95de51f
kudongku <kudongku@naver.com>
2024-10-12 17:15:32 +0900

    Style : part isort, black 적용

--- app/api/main.py
+++ app/api/main.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.routes.annual_leaves import annual_leaves
 from app.api.routes.auth import auth
+from app.api.routes.parts import parts
 
 # from app.api.routes.overtimes import overtimes
 
@@ -11,4 +12,5 @@ app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(
     annual_leaves.router, prefix="/annual-leaves", tags=["Annual Leaves"]
 )
+app.include_router(parts.router, prefix="/parts", tags=["Parts"])
 # app.include_router(overtimes.router, prefix='/overtime', tags=['Overtime'])
