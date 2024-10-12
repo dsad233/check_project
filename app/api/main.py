4b48ad73a04bb051f7e941bb4fc0768d4450f70f
devdotcor <ksl1421@mement.ai>
2024-10-13 02:33:46 +0900

    Fix : Git 충돌 오류 해결

--- app/api/main.py
+++ app/api/main.py
@@ -4,6 +4,7 @@ from app.api.routes.auth import auth
 from app.api.routes.parts import parts
 from app.api.routes.users import users
 from app.api.routes.branch_policies import branch_policies
+from app.api.routes.parts import parts
 
 # from app.api.routes.annual_leaves import annual_leaves
 # from app.api.routes.overtimes import overtimes
@@ -16,5 +17,5 @@ app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(
     branch_policies.router, prefix="/branch_policies", tags=["Branch_policies"]
 )
-# app.include_router(annual_leaves.router, prefix='/annual-leaves', tags=['Annual Leaves'])
+app.include_router(parts.router, prefix="/parts", tags=["Parts"])
 # app.include_router(overtimes.router, prefix='/overtime', tags=['Overtime'])
--- app/api/routes/parts/schema/partsSchema.py
+++ app/api/routes/parts/schema/partsSchema.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
+
 class PartCreate(BaseModel):
     name: str
     branch_id: int
+
 
 class PartResponse(BaseModel):
     id: int
