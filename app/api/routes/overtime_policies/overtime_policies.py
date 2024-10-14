dc39d1076106601d07a5701cb8d81248015e019c
devdotcor <ksl1421@mement.ai>
2024-10-14 22:00:49 +0900

    Fix : overtimes 수정

--- app/api/routes/overtime_policies/overtime_policies.py
+++ app/api/routes/overtime_policies/overtime_policies.py
@@ -6,7 +6,9 @@ from sqlalchemy.future import select
 
 from app.core.database import async_session
 from app.middleware.tokenVerify import validate_token
-from app.models.policies.branchpolicies import OverTimePolicies
+from app.models.branches.overtime_policies_model import OverTimePolicies, OverTimeCreate, OverTimeUpdate
+from app.models.branches.branches_model import Branches
+from app.models.parts.parts_model import Parts
 
 # from app.api.routes.overtimes.schema.overtimeschema import OverTimeCrete, OverTimeEdit
 
@@ -51,135 +53,102 @@ async def find_overtime_one (branch_id : int, id : int):
     except Exception as err:
         print(err)
         raise HTTPException(status_code= 500, detail="오버타임 정책 상세 조회에 에러가 발생하였습니다.")
-    
 
-# overtime 파트별 검색 조회
-@router.get("/{branch_id}/overtime_policies/{id}")
-async def find_overtime_one (branch_id : int, branch_policy_id : int, id : int):
+
+# overtime 파트명별 검색 조회
+@router.get("/{branch_id}/overtime_policies/part_name/{name}")
+async def find_overtime_one (branch_id : int, name : str):
     try:
-        overtime_one = await overtime.execute(select(OverTimePolicies).where(OverTimePolicies.branch_id == branch_id, OverTimePolicies.branch_policy_id == branch_policy_id, OverTimePolicies.id == id, OverTimePolicies.deleted_yn == "N"))
+        overtime_one = await overtime.execute(select(OverTimePolicies).where(OverTimePolicies.branch_id == branch_id, Parts.name.like(f"{name}%"), OverTimePolicies.deleted_yn == "N"))
         result = overtime_one.scalar_one_or_none()
 
         if result == None:
             raise HTTPException(status_code=404, detail="오버타임 정책이 존재하지 않습니다.")
         
         return {
-            "message": "성공적으로 오버타임 정책 상세 조회에 성공하였습니다.",
+            "message": "성공적으로 오버타임 정책 파트명 조회에 성공하였습니다.",
             "data": result
         }
     except Exception as err:
         print(err)
-        raise HTTPException(status_code= 500, detail="오버타임 정책 상세 조회에 에러가 발생하였습니다.")
+        raise HTTPException(status_code= 500, detail="오버타임 정책 파트명별 조회에 에러가 발생하였습니다.")
 
     
 
-# overtime 지점애 따라 조회
-@router.get("/{branch_id}/overtime_policies/{id}")
-async def find_spot(spot: str):
+# overtime 지점명에 따라 조회
+@router.get("/{branch_id}/overtime_policies/branches/{name}")
+async def find_spot(branch_id : int, name : str):
     try:
         find_spot = await overtime.execute(
-            select(OverTimePolicies).where(OverTimePolicies.overtime_id == id)
+            select(OverTimePolicies).where(Branches.name.like(f"{name}%"), OverTimePolicies.branch_id == branch_id, OverTimePolicies.deleted_yn == "N")
         )
         result = find_spot.scalar_one_or_none()
 
         if result == None:
-            return JSONResponse(
-                status_code=404, content="타임 데이터가 존재하지 않습니다."
-            )
+            raise HTTPException(status_code=404, detail="오버타임 정책이 존재하지 않습니다.")
 
         return {
-            "message": "지점별 오버타임 전체 조회에 성공하였습니다.",
-            "data": result,
+            "message": "성공적으로 오버타임 정책 지점명별 조회에 성공하였습니다.",
+            "data": result
         }
     except Exception as err:
-        print("에러가 발생하였습니다.")
         print(err)
+        raise HTTPException(status_code= 500, detail="오버타임 정책 지점명별 조회에 에러가 발생하였습니다.")
 
 
-# overtime 상세 조회
-@router.get("/{id}")
-async def find_one(id: int):
+# overtime 생성 [어드민만]
+@router.post("/{branch_id}/overtime_policies")
+async def time_create(overTimeCreate : OverTimeCreate, branch_id : int, token: Annotated[OverTimePolicies, Depends(validate_token)]):
     try:
-        overtimeone = await overtime.execute(
-            select(OverTimePolicies).where(OverTimePolicies.overtime_id == id)
+        if token.role != "MSO 최고권한" | token.role != "최고관리자":
+            raise HTTPException(status_code=403, detail="생성 권한이 없습니다.")
+        
+        
+        create = OverTimePolicies(
+            name = overTimeCreate.name,
+            ot_30 = overTimeCreate.ot_30,
+            ot_60 = overTimeCreate.ot_60,
+            ot_90 = overTimeCreate.ot_90,
+            ot_120 = overTimeCreate.ot_120
         )
-        result = overtimeone.scalar_one_or_none()
+        
+        await overtime.add(create)
+        await overtime.commit()
+        await overtime.refresh(create)
+
+        return {"message": "성공적으로 오버타임 정책 생성에 성공하였습니다."}
+    except Exception as err:
+        print(err)
+        raise HTTPException(status_code= 500, detail="오버타임 정책 생성에 에러가 발생하였습니다.")
+
+
+# overtime 수정 [어드민만]
+@router.patch("/{branch_id}/overtime_policies/{id}")
+async def time_edit(branch_id : int, id: int, overTimeUpdate : OverTimeUpdate, token: Annotated[OverTimePolicies, Depends(validate_token)]):
+    try:
+        if token.role != "MSO 최고권한" | token.role != "최고관리자":
+            raise HTTPException(status_code=403, detail="생성 권한이 없습니다.")
+
+        find_one_over_time = await overtime.execute(select(OverTimePolicies).where(OverTimePolicies.branch_id == branch_id, OverTimePolicies.id == id, OverTimePolicies.deleted_yn == "N"))
+        result = find_one_over_time.scalar_one_or_none()
 
         if result == None:
             return JSONResponse(
                 status_code=404, content="타임 데이터가 존재하지 않습니다."
             )
 
-        return {
-            "message": "오버타임 데이터 상세 조회에 성공하였습니다.",
-            "data": result,
-        }
+        result.name = overTimeUpdate.name
+        result.ot_30 = overTimeUpdate.ot_30
+        result.ot_60 = overTimeUpdate.ot_60
+        result.ot_90 = overTimeUpdate.ot_90
+        result.ot_120 = overTimeUpdate.ot_120
+
+        await overtime.commit()
+
+        return {"message": "성공적으로 오버타임 정책 수정에 성공하였습니다."}
     except Exception as err:
-        print("에러가 발생하였습니다.")
         print(err)
-
-
-# overtime 생성
-@router.post("")
-async def time_create(token: Annotated[OverTimePolicies, Depends(validate_token)]):
-    try:
-
-        # overtimecreate = Overtime(
-        #     proposer_id = token.id,
-        #     application_note = schema.application_note
-        # )
-
-        # overtime.add(overtimecreate)
-        # overtime.commit()
-        # overtime.refresh(overtimecreate)
-
-        return {"message": "오버타임 데이터 요청에 성공하였습니다."}
-    except Exception as err:
-        print("에러가 발생하였습니다.")
-        print(err)
-
-
-# overtime 수정 [어드민만]
-@router.patch("/{id}")
-async def time_edit(id: int, token: Annotated[OverTimePolicies, Depends(validate_token)]):
-    try:
-
-        find_over_time = overtime.query(OverTimePolicies).filter(OverTimePolicies.id == id).first()
-
-        if find_over_time == None:
-            return JSONResponse(
-                status_code=404, content="타임 데이터가 존재하지 않습니다."
-            )
-
-        # find_over_time.manager_id = token.id
-        # find_over_time.proposer_note = schema.proposer_note
-        # find_over_time.processed_date = schema.processed_date
-        # find_over_time.application_status = schema.application_status
-
-        return {"message": "오버타임 데이터 업데이트에 성공하였습니다."}
-    except Exception as err:
-        print("에러가 발생하였습니다.")
-        print(err)
-
-
-# overtime 데이터 소프트 삭제 [어드민만]
-@router.delete("/{id}")
-async def time_soft_delete(id: int):
-    try:
-        find_over_time = overtime.query(OverTimePolicies).filter(OverTimePolicies.id == id).first()
-
-        if find_over_time == None:
-            return JSONResponse(
-                status_code=404, content="타임 데이터가 존재하지 않습니다."
-            )
-
-        overtime.deleted_yn = "Y"
-
-        return {"message": "오버타임 데이터를 정상적으로 삭제하였습니다."}
-    except Exception as err:
-        print("에러가 발생하였습니다.")
-        print(err)
-
+        raise HTTPException(status_code= 500, detail="오버타임 정책 수정에 에러가 발생하였습니다.")
 
 # overtime 데이터 삭제 [어드민만]
 @router.delete("/{id}")
@@ -199,3 +168,23 @@ async def time_delete(id: int):
     except Exception as err:
         print("에러가 발생하였습니다.")
         print(err)
+
+# overtime 데이터 소프트 삭제 [어드민만]
+@router.delete("/{branch_id}/overtime_policies/{id}")
+async def time_soft_delete(id: int):
+    try:
+        find_over_time = overtime.query(OverTimePolicies).filter(OverTimePolicies.id == id).first()
+
+        if find_over_time == None:
+            return JSONResponse(
+                status_code=404, content="타임 데이터가 존재하지 않습니다."
+            )
+
+        overtime.deleted_yn = "Y"
+
+        return {"message": "오버타임 데이터를 정상적으로 삭제하였습니다."}
+    except Exception as err:
+        print("에러가 발생하였습니다.")
+        print(err)
+
+
