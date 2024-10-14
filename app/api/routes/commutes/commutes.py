bb0998cf3c07570c7df10d2fc05ff6707797dae5
jkc09709 <jkc9709@mement.ai>
2024-10-14 11:44:35 +0900

    feat/get commute records api

--- app/api/routes/commutes/commutes.py
+++ app/api/routes/commutes/commutes.py
@@ -1,4 +1,6 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import func, select
 from app.middleware.tokenVerify import validate_token, get_current_user_id
 from app.api.routes.commutes.schema.commute_schema import CommuteCreate, CommuteResponse
 from app.core.database import async_session
@@ -7,7 +9,9 @@ from app.models.models import Commutes
 router = APIRouter(dependencies=[Depends(validate_token)])
 db = async_session()
 
-@router.post("", response_model=CommuteResponse)
+
+# 출퇴근 기록 생성
+@router.post("")
 async def create_clock_in(
     commute: CommuteCreate,
     current_user_id: int = Depends(get_current_user_id)
@@ -22,17 +26,50 @@ async def create_clock_in(
         await db.commit()
         await db.refresh(new_commute)
 
-        return CommuteResponse(
-            id=new_commute.id,
-            user_id=new_commute.user_id,
-            clock_in=new_commute.clock_in,
-            clock_out=new_commute.clock_out,
-            work_hours=new_commute.work_hours,
-            updated_at=new_commute.updated_at,
-            deleted_yn=new_commute.deleted_yn
-        )
+        return {
+            "message": "출근 기록이 성공적으로 생성되었습니다.",
+            "data": new_commute,
+        }
+    
     except Exception as err:
         await db.rollback()
+        print("에러가 발생하였습니다.")
+        print(err)
+        raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다.")
+
+
+# 출퇴근 기록 목록 조회
+@router.get("")
+async def get_commute_records(
+    current_user_id: int = Depends(get_current_user_id),
+    skip: int = 0,
+    limit: int = 100
+):
+    try:
+        # 전체 출퇴근 기록 수 조회
+        count_query = select(func.count()).select_from(Commutes).where(Commutes.deleted_yn == "N")
+        total_count = await db.execute(count_query)
+        total_count = total_count.scalar_one()
+
+        stmt = (
+            select(Commutes)
+            .where((Commutes.user_id == current_user_id) & (Commutes.deleted_yn == "N"))
+            .order_by(Commutes.clock_in.desc())
+            .offset(skip)
+            .limit(limit)
+        )
+        result = await db.execute(stmt)
+        commutes = result.scalars().all()
+
+        return {
+            "message": "출퇴근 기록을 정상적으로 전체 조회를 완료하였습니다.",
+            "data": commutes,
+            "total": total_count,
+            "skip": skip,
+            "limit": limit
+        }
+    
+    except Exception as err:
         print("에러가 발생하였습니다.")
         print(err)
         raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다.")
