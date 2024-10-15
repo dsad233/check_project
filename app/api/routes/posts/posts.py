c1e868da70e8cd5f830e203ca6f853d12294b9c1
kudongku <kudongku@naver.com>
2024-10-15 21:26:43 +0900

    Feat : posts 검색 기능 구현

--- app/api/routes/posts/posts.py
+++ app/api/routes/posts/posts.py
@@ -1,22 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select, func
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, aliased
 from datetime import datetime
+
 from app.core.database import async_session
 from app.middleware.tokenVerify import validate_token, get_current_user_id
-from app.models.users.posts_model import Posts, PostsResponse, PostListResponse, PostsCreate, PostsUpdate
+from app.models.users.posts_model import Posts, PostsResponse, PostListResponse, PostsCreate, PostsUpdate, PostsSearchDto
 from app.models.users.users_model import Users
 from app.models.branches.board_model import Board
-from app.common.dto.search_dto import BaseSearchDto
 from app.common.dto.pagination_dto import PaginationDto
 
 router = APIRouter(dependencies=[Depends(validate_token)])
 db = async_session()
 
-# 게시글 전체 조회
 @router.get("", response_model=PostListResponse)
-async def getAllPost(
-    branch_id: int, board_id: int, search: BaseSearchDto = Depends(), current_user_id: int = Depends(get_current_user_id)
+async def get_posts(
+    branch_id: int, board_id: int, search: PostsSearchDto = Depends(), current_user_id: int = Depends(get_current_user_id)
 ) -> PostListResponse:
     try:
         user_query = select(Users).where(Users.id == current_user_id, Users.deleted_yn == 'N')
@@ -25,7 +24,7 @@ async def getAllPost(
         
         if current_user.role.strip() != "MSO 최고권한" and current_user.branch_id != branch_id :
             raise HTTPException(status_code=403, detail="권한이 없습니다.")
-                
+
         board_query = select(Board).where(Board.id == board_id, Board.branch_id == branch_id, Board.deleted_yn == 'N')
         board_result = await db.execute(board_query)
         board = board_result.scalar_one_or_none()
@@ -38,10 +37,23 @@ async def getAllPost(
         count_query = select(func.count()).select_from(Posts).where(Posts.branch_id == branch_id, Posts.board_id == board_id, Posts.deleted_yn == 'N')
         count_result = await db.execute(count_query)
         count = count_result.scalar_one_or_none()
-                
+        
         pagination = PaginationDto(total_record=count)
 
-        query = select(Posts).options(joinedload(Posts.users)).offset(search.offset).limit(search.record_size).where(Posts.branch_id == branch_id, Posts.board_id == board_id, Posts.deleted_yn == 'N')
+
+        query = select(Posts).join(Users, Posts.user_id == Users.id).options(joinedload(Posts.users)).offset(search.offset).limit(search.record_size).where(
+            Posts.branch_id == branch_id, 
+            Posts.board_id == board_id, 
+            Posts.deleted_yn == 'N'
+        )
+        
+        if search.title is not None:
+            query = query.where(Posts.title.like(f"%{search.title}%"))
+        if search.content is not None:
+            query = query.where(Posts.content.like(f"%{search.content}%"))
+        if search.author_name is not None:
+            query = query.where(Users.name.like(f"%{search.author_name}%"))
+        
         result = await db.execute(query)
         getPost = result.scalars().all()
 
@@ -60,10 +72,8 @@ async def getAllPost(
         print("에러가 발생하였습니다.", err)
         raise HTTPException(status_code=500, detail=str(err))
 
-
-# 게시글 상세 조회
 @router.get("/{post_id}", response_model=PostsResponse)
-async def getOnePost(
+async def get_post(
     branch_id: int, board_id: int, post_id: int, current_user_id: int = Depends(get_current_user_id)
 ) -> PostsResponse:
     try:
@@ -73,11 +83,11 @@ async def getOnePost(
         
         if current_user.role.strip() != "MSO 최고권한" and current_user.branch_id != branch_id :
             raise HTTPException(status_code=403, detail="권한이 없습니다.")
-                
+        
         board_query = select(Board).where(Board.id == board_id, Board.branch_id == branch_id, Board.deleted_yn == 'N')
         board_result = await db.execute(board_query)
         board = board_result.scalar_one_or_none()
-
+        
         check_authority(current_user.role.strip(), board.read_authority.strip())
 
         query = select(Posts).where(Posts.id == post_id, Posts.branch_id == branch_id, Posts.board_id == board_id, Posts.deleted_yn == 'N')
@@ -100,10 +110,8 @@ async def getOnePost(
         print("에러가 발생하였습니다.", err)
         raise HTTPException(status_code=500, detail=str(err))
 
-
-# 게시글 생성
-@router.post("")
-async def postCreate(
+@router.post("", status_code=201)
+async def create_post(
     branch_id: int, board_id: int, postCreate: PostsCreate, current_user_id: int = Depends(get_current_user_id)
 ):
     try:
@@ -137,10 +145,8 @@ async def postCreate(
         print("에러가 발생하였습니다.", err)
         raise HTTPException(status_code=500, detail=str(err))
 
-
-# 게시글 수정
 @router.patch("/{post_id}")
-async def postUpdate(
+async def update_post(
     branch_id: int, board_id: int, post_id: int, posts_update: PostsUpdate, current_user_id: int = Depends(get_current_user_id)
 ):
     try:
@@ -180,10 +186,8 @@ async def postUpdate(
         print("에러가 발생하였습니다.", err)
         raise HTTPException(status_code=500, detail=str(err))
 
-
-# 게시글 삭제
-@router.delete("/{id}")
-async def postDelete(
+@router.delete("/{post_id}")
+async def delete_post(
     branch_id: int, board_id: int, post_id: int, current_user_id: int = Depends(get_current_user_id)
 ):
     try:
@@ -219,19 +223,10 @@ async def postDelete(
         print("에러가 발생하였습니다.", err)
         raise HTTPException(status_code=500, detail=str(err))
 
-def check_authority(user_role_enum:str, board_role_enum:str):
+def check_authority(user_role:str, board_role:str):
     
-    if board_role_enum.strip() == 'MSO 최고권한':
-        if user_role_enum.strip() != 'MSO 최고권한':
-            raise HTTPException(status_code=403, detail="권한이 없습니다.")
-        
-    if board_role_enum.strip() == '최고관리자':
-        if user_role_enum.strip() not in ['최고관리자', 'MSO 최고권한']:
-            raise HTTPException(status_code=403, detail="권한이 없습니다.")
-        
-    if board_role_enum.strip() == '관리자':
-        if user_role_enum.strip() not in ['관리자', '최고관리자', 'MSO 최고권한']:
-            raise HTTPException(status_code=403, detail="권한이 없습니다.")
-    if board_role_enum.strip() == '사원':
-        if user_role_enum.strip() == '퇴사자':
-            raise HTTPException(status_code=403, detail="권한이 없습니다.")
+    if (board_role == 'MSO 최고권한' and user_role != 'MSO 최고권한') or \
+       (board_role == '최고관리자' and user_role not in ['최고관리자', 'MSO 최고권한']) or \
+       (board_role == '관리자' and user_role not in ['관리자', '최고관리자', 'MSO 최고권한']) or \
+       (user_role == '퇴사자'):
+        raise HTTPException(status_code=403, detail="권한이 없습니다.")
