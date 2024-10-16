c922197180fddcc9723f595ed371f94457c6f6f5
bboodd <gkrgus9@gmail.com>
2024-10-16 20:41:30 +0900

    fix: model 관계, import, 중복overtime_policies 주석처리

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -24,6 +24,9 @@ from .branches.rest_days_model import RestDays
 from .users.leave_histories_model import LeaveHistories
 from .branches.leave_categories_model import LeaveCategories
 
+# parts
+from .parts.hour_wage_template_model import HourWageTemplate
+
 # 여기서 관계를 설정합니다
 # 일 대 다 관계
 Branches.auto_overtime_policies = relationship("AutoOvertimePolicies", back_populates="branch")
@@ -39,11 +42,13 @@ Branches.closed_days = relationship("ClosedDays", back_populates="branch")
 Branches.leave_histories = relationship("LeaveHistories", back_populates="branch")
 Branches.work_policies = relationship("WorkPolicies", back_populates="branch")
 Branches.allowance_policies = relationship("AllowancePolicies", back_populates="branch")
+Branches.hour_wage_templates = relationship("HourWageTemplate", back_populates="branch")
 
 LeaveCategories.leave_histories = relationship("LeaveHistories", back_populates="leave_category")
 
 Parts.salary_policies = relationship("SalaryPolicies", back_populates="part")
 Parts.users = relationship("Users", back_populates="part")
+Parts.hour_wage_templates = relationship("HourWageTemplate", back_populates="part")
 
 Users.leave_histories = relationship("LeaveHistories", back_populates="user")
 Users.applied_overtimes = relationship("Overtimes", foreign_keys=[Overtimes.applicant_id], back_populates="applicant")
@@ -76,3 +81,6 @@ ClosedDays.branch = relationship("Branches", back_populates="closed_days")
 Overtimes.applicant = relationship("Users", foreign_keys=[Overtimes.applicant_id], back_populates="applied_overtimes")
 Overtimes.manager = relationship("Users", foreign_keys=[Overtimes.manager_id], back_populates="managed_overtimes")
 LeaveHistories.branch = relationship("Branches", back_populates="leave_histories")
+
+HourWageTemplate.branch = relationship("Branches", back_populates="hour_wage_templates")
+HourWageTemplate.part = relationship("Parts", back_populates="hour_wage_templates")
