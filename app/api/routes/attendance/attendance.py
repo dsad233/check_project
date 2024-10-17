6b3d3069bc1fc93d9efb8d5fbe50fe1bd45e76fe
devdotcor <ksl1421@mement.ai>
2024-10-17 10:15:10 +0900

    Fix : Git 충돌 오류 해결

--- app/api/routes/attendance/attendance.py
+++ app/api/routes/attendance/attendance.py
@@ -5,7 +5,7 @@ from app.core.database import async_session
 from app.models.branches.branches_model import Branches
 from app.models.parts.parts_model import Parts
 from app.models.users.users_model import Users
-from app.middleware.tokenVerify import validate_token
+from app.middleware.tokenVerify import validate_token, get_current_user
 
 
 router = APIRouter(dependencies=[Depends(validate_token)])
@@ -13,7 +13,7 @@ attendance = async_session()
 
 # 근로 관리 전체 조회
 @router.get('/{branch_id}/parts/{part_id}/attendance')
-async def find_attendance(branch_id : int, part_id : int, token=Annotated[Users, Depends(validate_token)]):
+async def find_attendance(branch_id : int, part_id : int, token=Annotated[Users, Depends(get_current_user)]):
     try:
         find_attendance = await attendance.execute(select(Users))
     except Exception as err:
