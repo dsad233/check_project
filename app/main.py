3846d51122f6854429158aef2fb5245e52f43724
joosomi <wnthal1211@gmail.com>
2024-10-28 15:29:20 +0900

    feat: 토큰 검증/permission 미들웨어 생성 및 api/login, api/logout 주석 처리
    - auth/login, auth/logout과 중복되기 때문에 주석 처리하였음.

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ origins = [
 # 1. Permission 미들웨어 (마지막에 실행)
 # 2. CORS 미들웨어 (먼저 실행)
 app.add_middleware(
-    CORSMiddleware,
+    CORSMiddleware,  # type: ignore
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
