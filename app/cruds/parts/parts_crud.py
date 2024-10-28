2a05a8edfba40db449602d9d5d2774c062823635
bboodd <gkrgus9@gmail.com>
2024-10-28 15:29:22 +0900

    fix: 연차 자동부여 설정 테이블 분리

--- app/cruds/parts/parts_crud.py
+++ app/cruds/parts/parts_crud.py
@@ -8,10 +8,11 @@ from sqlalchemy.exc import SQLAlchemyError, NoResultFound
 from app.common.dto.search_dto import BaseSearchDto
 from app.models.parts.parts_model import Parts
 from app.exceptions.exceptions import InvalidEnumValueError, NotFoundError
+from app.enums.parts import PartAutoAnnualLeaveGrant
+
 
 logger = logging.getLogger(__name__)
 
-VALID_AUTO_ANNUAL_LEAVE_GRANTS = ["회계기준 부여", "입사일 기준 부여", "조건별 부여", "수동부여"]
 
 async def find_by_id_and_branch_id(
     *, session: AsyncSession, branch_id: int, part_id: int
@@ -28,7 +29,7 @@ async def find_by_id_and_branch_id(
     return part
 
 async def update_auto_annual_leave_grant(
-    *, session: AsyncSession, branch_id: int, part_id: int, auto_annual_leave_grant: str
+    *, session: AsyncSession, branch_id: int, part_id: int, auto_annual_leave_grant: PartAutoAnnualLeaveGrant
 ) -> Parts:
         
     # 먼저 해당 Parts가 존재하는지 확인
@@ -36,16 +37,16 @@ async def update_auto_annual_leave_grant(
     if not part:
         raise NotFoundError(f"{branch_id}번 지점의 {part_id}번 파트가 존재하지 않습니다.")
     
-        # Enum 값 검증
-    if auto_annual_leave_grant not in VALID_AUTO_ANNUAL_LEAVE_GRANTS:
+    # Enum 값 검증
+    if auto_annual_leave_grant not in PartAutoAnnualLeaveGrant:
         raise InvalidEnumValueError(f"잘못된 자동 부여 정책 ENUM 입니다: {auto_annual_leave_grant}")
 
     # 현재 값과 비교하여 변경이 필요한 경우에만 업데이트
-    if part.auto_annual_leave_grant != auto_annual_leave_grant:
+    if part.auto_annual_leave_grant != auto_annual_leave_grant.value:
         await session.execute(
             update(Parts)
             .where(Parts.id == part_id)
-            .values(auto_annual_leave_grant=auto_annual_leave_grant)
+            .values(auto_annual_leave_grant=auto_annual_leave_grant.value)
         )
 
         await session.commit()
