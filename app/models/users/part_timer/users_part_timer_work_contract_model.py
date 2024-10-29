c609082f84aeaca0b5f6bf7d60516a3c44f55a03
koesnam <koesnam@mement.ai>
2024-10-29 18:22:09 +0900

    fix: 파트타이머 관련 4개 테이블 생성 적용

--- app/models/users/part_timer/users_part_timer_work_contract_model.py
+++ app/models/users/part_timer/users_part_timer_work_contract_model.py
@@ -3,7 +3,7 @@ from app.core.database import Base
 
 
 class PartTimerWorkContract(Base):
-    __tablename__ = "part_timer_work_contract"
+    __tablename__ = "part_timer_work_contracts"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
@@ -11,19 +11,19 @@ class PartTimerWorkContract(Base):
     contract_end_date = Column(Date, nullable=False)
 
 class PartTimerWorkingTime(Base):
-    __tablename__ = "part_timer_working_time"
+    __tablename__ = "part_timer_working_times"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    part_timer_work_contract_id = Column(Integer, ForeignKey("part_timer_work_contract.id"), nullable=False)
+    part_timer_work_contract_id = Column(Integer, ForeignKey("part_timer_work_contracts.id"), nullable=False)
     day_of_week = Column(Integer, nullable=False) # 0: 일요일, 1: 월요일, 2: 화요일, 3: 수요일, 4: 목요일, 5: 금요일, 6: 토요일
     work_start_time = Column(Time, nullable=False)
     work_end_time = Column(Time, nullable=False)
 
 class PartTimerHourlyWage(Base):
-    __tablename__ = "part_timer_hourly_wage"
+    __tablename__ = "part_timer_hourly_wages"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    part_timer_work_contract_id = Column(Integer, ForeignKey("part_timer_work_contract.id"), nullable=False)
+    part_timer_work_contract_id = Column(Integer, ForeignKey("part_timer_work_contracts.id"), nullable=False)
     calculate_start_time = Column(Time, nullable=False)
     calculate_end_time = Column(Time, nullable=False)
     hourly_wage = Column(Integer, nullable=False)
@@ -32,10 +32,10 @@ class PartTimerHourlyWage(Base):
 출퇴근 기록 별로 추가 정보
 '''
 class PartTimerAdditionalInfo(Base):
-    __tablename__ = "part_timer_additional_info"
+    __tablename__ = "part_timer_additional_infos"
 
     commute_id = Column(Integer, ForeignKey("commutes.id"), primary_key=True, nullable=False)
-    part_details = Column(String(255), nullable=False)
+    work_type = Column(String(255), nullable=False)
     rest_minutes = Column(Integer, nullable=False, default=30)
     
 
