e9506d8e34af81da3eabe5f0153bb5c2d3a2f96b
ysk9526 <ysk9526@mement.ai>
2024-10-29 18:03:53 +0900

    FIX : 마이그레이션 버전 미동기화 수정

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,7 @@ class BaseAppSettings(BaseSettings):
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
     # MODUSIGN_API_KEY: str = Field(..., env="MODUSIGN_API_KEY")
-    # MODUSIGN_USER_EMAIL: str  
+    # MODUSIGN_USER_EMAIL: str
     
     @property
     def MODUSIGN_HEADERS(self) -> dict:
