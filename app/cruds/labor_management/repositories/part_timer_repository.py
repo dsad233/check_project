29475fed03fe3ff72a8e93c5a85cdd07163ea5cd
koesnam <koesnam@mement.ai>
2024-10-31 10:18:05 +0900

    docs: 메서드 정보 추가
    메서드별 동작을 설명하는 주석 추가

--- app/cruds/labor_management/repositories/part_timer_repository.py
+++ app/cruds/labor_management/repositories/part_timer_repository.py
@@ -19,7 +19,6 @@ from app.models.parts.parts_model import Parts
 from app.models.commutes.commutes_model import Commutes
 from app.models.users.part_timer.users_part_timer_work_contract_model import PartTimerAdditionalInfo, PartTimerHourlyWage, PartTimerWorkContract
 
-# TODO: 실제 DB 레포지토리 구현
 class PartTimerRepository(IPartTimerRepository):
     def __init__(self, session: AsyncSession):
         self.session = session
@@ -97,6 +96,18 @@ class PartTimerRepository(IPartTimerRepository):
         return [PartTimerSummaryResponseDTO(**record._asdict()) for record in result.all()]
 
     async def get_all_part_timers(self, year: int, month: int, page_num: int, page_size: int) -> List[PartTimerSummaryResponseDTO]:
+        '''
+        모든 파트 타이머의 요약 정보를 페이지네이션하여 가져옵니다.
+
+        Args:
+            year (int): 연도
+            month (int): 월
+            page_num (int): 페이지 번호
+            page_size (int): 페이지 크기
+
+        Returns:
+            List[PartTimerSummaryResponseDTO]: 파트 타이머들의 요약 정보 리스트
+        '''
         part_timer_find_subquery = (
             select(Users.id)
                 .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -114,6 +125,17 @@ class PartTimerRepository(IPartTimerRepository):
     특정 직원의 근무 내역 조회
     '''
     async def get_part_timer_work_histories(self, user_id: int, year: int, month: int) -> List[PartTimerWorkHistoryDTO]:
+        '''
+        특정 직원의 근무 내역을 조회합니다.
+
+        Args:
+            user_id (int): 직원의 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            List[PartTimerWorkHistoryDTO]: 직원의 근무 내역 리스트
+        '''
         query = (self.get_part_timer_work_history_summary_select_query()
             .select_from(Users)
             .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -143,6 +165,19 @@ class PartTimerRepository(IPartTimerRepository):
         return [PartTimerWorkHistoryDTO(**record._asdict()) for record in records]
     
     async def get_all_part_timers_by_branch_id(self, branch_id: int, year: int, month: int, page_num: int, page_size: int) -> List[PartTimerSummaryResponseDTO]:
+        '''
+        특정 지점의 모든 파트 타이머의 요약 정보를 페이지네이션하여 가져옵니다.
+
+        Args:
+            branch_id (int): 지점 ID
+            year (int): 연도
+            month (int): 월
+            page_num (int): 페이지 번호
+            page_size (int): 페이지 크기
+
+        Returns:
+            List[PartTimerSummaryResponseDTO]: 파트 타이머들의 요약 정보 리스트
+        '''
         part_timer_find_subquery = (
             select(Users.id)
                 .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -158,6 +193,20 @@ class PartTimerRepository(IPartTimerRepository):
         return await self.calculate_hours_and_wage(year, month, part_timer_find_subquery)
     
     async def get_all_part_timers_by_branch_id_and_part_id(self, branch_id: int, part_id: int, year: int, month: int, page_num: int, page_size: int) -> List[PartTimerSummaryResponseDTO]:
+        '''
+        특정 지점 및 파트의 모든 파트 타이머의 요약 정보를 페이지네이션하여 가져옵니다.
+
+        Args:
+            branch_id (int): 지점 ID
+            part_id (int): 파트 ID
+            year (int): 연도
+            month (int): 월
+            page_num (int): 페이지 번호
+            page_size (int): 페이지 크기
+
+        Returns:
+            List[PartTimerSummaryResponseDTO]: 파트 타이머들의 요약 정보 리스트
+        '''
         part_timer_find_subquery = (
             select(Users.id)
                 .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -174,6 +223,17 @@ class PartTimerRepository(IPartTimerRepository):
         return await self.calculate_hours_and_wage(year, month, part_timer_find_subquery)
     
     async def get_part_timer_work_history_summary_by_user_id(self, user_id: int, year: int, month: int) -> PartTimerWorkHistorySummaryDTO:
+        '''
+        특정 직원의 근무 내역 요약 정보를 조회합니다.
+
+        Args:
+            user_id (int): 직원의 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            PartTimerWorkHistorySummaryDTO: 직원의 근무 내역 요약 정보
+        '''
         work_hours = func.TIME_TO_SEC(
             func.TIMEDIFF(
                 PartTimerAdditionalInfo.work_set_end_time,
@@ -226,6 +286,20 @@ class PartTimerRepository(IPartTimerRepository):
         return PartTimerWorkHistorySummaryDTO(**result.first()._asdict())
     
     async def get_part_timer_by_user_info(self, year: int, month: int, user_name: str | None, phone_number: str | None, branch_id: int, part_id: int) -> List[PartTimerSummaryResponseDTO]:
+        '''
+        사용자 이름 또는 전화번호로 파트 타이머를 검색하여 요약 정보를 가져옵니다.
+
+        Args:
+            year (int): 연도
+            month (int): 월
+            user_name (str | None): 사용자 이름 (옵션)
+            phone_number (str | None): 전화번호 (옵션)
+            branch_id (int): 지점 ID
+            part_id (int): 파트 ID
+
+        Returns:
+            List[PartTimerSummaryResponseDTO]: 검색된 파트 타이머들의 요약 정보 리스트
+        '''
         condition = []
         if user_name:
             condition.append(Users.name.ilike(f'%{user_name}%'))
@@ -247,6 +321,17 @@ class PartTimerRepository(IPartTimerRepository):
         return await self.calculate_hours_and_wage(year, month, part_timer_find_subquery)
 
     async def update_part_timer_work_history(self, commute_id: int, correction_data: PartTimerCommuteHistoryCorrectionRequestDTO) -> PartTimerCommuteHistoryCorrectionResponseDTO:       
+        '''
+        특정 근무내역을 수정합니다.
+        * 프론트에서 모든 내역을 다시 불러오기 때문에 총 급여는 따로 계산하지 않고 있습니다.
+        
+        Args:
+            commute_id (int): 근무내역 ID
+            correction_data (PartTimerCommuteHistoryCorrectionRequestDTO): 수정할 데이터
+
+        Returns:
+            PartTimerCommuteHistoryCorrectionResponseDTO: 수정된 근무내역 정보
+        '''
         response = PartTimerCommuteHistoryCorrectionResponseDTO.Builder(commute_id, correction_data) \
             .set_work_time(correction_data.work_start_set_time, correction_data.work_end_set_time) \
             .set_rest_minutes(correction_data.rest_minutes) \
@@ -269,11 +354,30 @@ class PartTimerRepository(IPartTimerRepository):
         return response
     
     async def exist_part_timer_work_history(self, commute_id: int) -> bool:
+        '''
+        특정 근무내역이 존재하는지 확인합니다.
+
+        Args:
+            commute_id (int): 근무내역 ID
+
+        Returns:
+            bool: 근무내역 존재 여부
+        '''
         query = select(exists().where(PartTimerAdditionalInfo.commute_id == commute_id))
         result = await self.session.execute(query)
         return result.scalar()
     
     async def get_total_count_part_timers(self, year: int, month: int) -> int:
+        '''
+        주어진 연도와 월에 해당하는 모든 파트 타이머의 총 수를 반환합니다.
+
+        Args:
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            int: 파트 타이머의 총 수
+        '''
         query = (
             select(func.count(Users.id))
                 .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -293,6 +397,17 @@ class PartTimerRepository(IPartTimerRepository):
         return result.scalar()
     
     async def get_total_count_part_timers_by_branch_id(self, branch_id: int, year: int, month: int) -> int:
+        '''
+        특정 지점의 주어진 연도와 월에 해당하는 파트 타이머의 총 수를 반환합니다.
+
+        Args:
+            branch_id (int): 지점 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            int: 파트 타이머의 총 수
+        '''
         query = (
             select(func.count(Users.id))
                 .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -313,6 +428,18 @@ class PartTimerRepository(IPartTimerRepository):
         return result.scalar()
     
     async def get_total_count_part_timers_by_branch_id_and_part_id(self, branch_id: int, part_id: int, year: int, month: int) -> int:
+        '''
+        특정 지점 및 파트의 주어진 연도와 월에 해당하는 파트 타이머의 총 수를 반환합니다.
+
+        Args:
+            branch_id (int): 지점 ID
+            part_id (int): 파트 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            int: 파트 타이머의 총 수
+        '''
         query = (
             select(func.count(Users.id))
                 .join(PartTimerWorkContract, PartTimerWorkContract.user_id == Users.id)
@@ -336,6 +463,12 @@ class PartTimerRepository(IPartTimerRepository):
     # ----------------------------------------------------------------------------------------------------------------------------
 
     def get_summary_select_query(self) -> Select:
+        '''
+        파트 타이머 요약 정보를 가져오는 Select 쿼리를 반환합니다.
+
+        Returns:
+            Select: 파트 타이머 요약 정보 쿼리
+        '''
         return select(
             Users.id.label('user_id'),
             Users.gender.label('gender'),
@@ -352,6 +485,12 @@ class PartTimerRepository(IPartTimerRepository):
     근무 내역 조회 Select Query
     '''
     def get_part_timer_work_history_summary_select_query(self) -> Select:
+        '''
+        근무 내역 조회를 위한 Select 쿼리를 반환합니다.
+
+        Returns:
+            Select: 근무 내역 조회 쿼리
+        '''
         work_hours = func.TIME_TO_SEC(
             func.TIMEDIFF(
                 PartTimerAdditionalInfo.work_set_end_time,
@@ -378,6 +517,16 @@ class PartTimerRepository(IPartTimerRepository):
     파트타이머 요약 정보 목록 조회
     '''
     async def get_part_timer_summaries(self, year: int, month: int):
+        '''
+        주어진 연도와 월에 해당하는 파트 타이머 요약 정보를 가져옵니다.
+
+        Args:
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            List: 파트 타이머 요약 정보 리스트
+        '''
         part_timer_summary_query = self.get_summary_select_query() \
             .join(Branches, Branches.id == Users.branch_id) \
             .filter(Users.employment_status == EmploymentStatus.TEMPORARY) \
@@ -394,6 +543,17 @@ class PartTimerRepository(IPartTimerRepository):
     지점별 파트타이머 요약 정보 목록 조회
     '''
     async def get_part_timer_summaries_by_branch_id(self, branch_id: int, year: int, month: int):
+        '''
+        특정 지점의 주어진 연도와 월에 해당하는 파트 타이머 요약 정보를 가져옵니다.
+
+        Args:
+            branch_id (int): 지점 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            List: 파트 타이머 요약 정보 리스트
+        '''
         part_timer_summary_query = self.get_summary_select_query() \
             .join(Branches, Branches.id == Users.branch_id) \
                 .filter(Branches.id == branch_id) \
@@ -411,6 +571,18 @@ class PartTimerRepository(IPartTimerRepository):
     지점별 및 파트별 파트타이머 요약 정보 목록 조회
     '''
     async def get_part_timer_summaries_by_branch_id_and_part_id(self, branch_id: int, part_id: int, year: int, month: int):
+        '''
+        특정 지점 및 파트의 주어진 연도와 월에 해당하는 파트 타이머 요약 정보를 가져옵니다.
+
+        Args:
+            branch_id (int): 지점 ID
+            part_id (int): 파트 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            List: 파트 타이머 요약 정보 리스트
+        '''
         part_timer_summary_query = self.get_summary_select_query() \
             .join(Branches, Branches.id == Users.branch_id) \
                 .filter(Branches.id == branch_id) \
@@ -429,6 +601,17 @@ class PartTimerRepository(IPartTimerRepository):
     특정 직원의 파트타이머 요약 정보 조회
     '''
     async def get_part_timer_summaries_by_user_id(self, user_id: int, year: int, month: int):
+        '''
+        특정 직원의 주어진 연도와 월에 해당하는 파트 타이머 요약 정보를 가져옵니다.
+
+        Args:
+            user_id (int): 직원 ID
+            year (int): 연도
+            month (int): 월
+
+        Returns:
+            List: 파트 타이머 요약 정보 리스트
+        '''
         part_timer_summary_query = self.get_summary_select_query() \
             .join(Branches, Branches.id == Users.branch_id) \
                 .filter(Users.id == user_id) \
