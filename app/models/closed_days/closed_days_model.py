cf19c71cfc6d2b55bea9cd9ca51f975316ab48a0
Jun Hyeok <yeoun@mement.ai>
2024-10-31 20:14:44 +0900

    FEAT: 지점 임시 휴점일 생성, 삭제

--- app/models/closed_days/closed_days_model.py
+++ app/models/closed_days/closed_days_model.py
@@ -1,12 +1,8 @@
-from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey
 from app.core.database import Base
-from datetime import date
-from typing import Optional
-
+from datetime import date, datetime
+from typing import Optional, List
 from pydantic import BaseModel, Field, field_validator
-from sqlalchemy.orm import relationship
-from datetime import datetime
-
 
 class ClosedDays(Base):
     __tablename__ = "closed_days"
@@ -21,21 +17,21 @@ class ClosedDays(Base):
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     deleted_yn = Column(String(1), default="N")
 
-    
+class ClosedDayItem(BaseModel):
+    closed_day_date: date
+    memo: Optional[str] = None
 
-
-class ClosedDayBase(BaseModel):
-
-    closed_day_date: Optional[date] = Field(None, description="휴무일 날짜")
-    memo: Optional[str] = Field(None, max_length=500, description="휴무일에 대한 메모")
+    model_config = {
+        "from_attributes": True
+    }
 
     @field_validator("closed_day_date")
     @classmethod
     def validate_date(cls, v):
-        if v is not None and v < date.today():
-            raise ValueError("휴무일은 오늘 이후의 날짜여야 합니다.")
+        if v < date.today():
+            raise ValueError("휴무일 등록과 삭제는 오늘 이후의 날짜여야 합니다.")
         return v
-
+    
     @field_validator("memo")
     @classmethod
     def validate_memo(cls, v):
@@ -43,14 +39,9 @@ class ClosedDayBase(BaseModel):
             raise ValueError("메모는 비어있을 수 없습니다.")
         return v
 
+class ClosedDayCreate(BaseModel):
+    closed_days: List[ClosedDayItem]
 
-class ClosedDayCreate(ClosedDayBase):
-    closed_day_date: date = Field(..., description="휴무일 날짜")
-    memo : Optional[str] = Field(None, description= "휴무일에 대한 메모")
-
-    class Config:
-        from_attributes = True
-
-
-class ClosedDayUpdate(ClosedDayBase):
-    pass
+    model_config = {
+        "from_attributes": True
+    }
