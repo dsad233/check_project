82fd77e53233964e6d64a626223b5c058bc7c2c6
Jun Hyeok <yeoun@mement.ai>
2024-11-03 17:53:47 +0900

    FEAT: 직원 조기 출근 등록,삭제,조회

--- app/api/routes/closed_days/dto/closed_days_response_dto.py
+++ app/api/routes/closed_days/dto/closed_days_response_dto.py
@@ -42,3 +42,25 @@ class EntireClosedDayResponseDTO(BaseModel):
     @classmethod
     def to_DTO(cls, user_closed_days: dict[str, List[UserClosedDayDetail]], hospital_closed_days: List[str]):
         return cls(user_closed_days=user_closed_days, hospital_closed_days=hospital_closed_days)
+
+class EarlyClockInResponseDTO(BaseModel):
+    user_id: int
+    user_name: str
+    part_name: str
+    category: str = "조기 출근"
+
+# 조기 출근 날짜와 직원 정보 조회
+class EarlyClockInListResponseDTO(BaseModel):
+    early_clock_in_days: dict[str, EarlyClockInResponseDTO]
+
+    @classmethod
+    def to_DTO(cls, early_clock_in_days: dict[str, EarlyClockInResponseDTO]):
+        return cls(early_clock_in_days=early_clock_in_days)
+
+# 조기 출근 날짜만 조회
+class SimpleEarlyClockInListResponseDTO(BaseModel):
+    early_clock_in_days: List[str]
+
+    @classmethod
+    def to_DTO(cls, early_clock_in_days: List[str]):
+        return cls(early_clock_in_days=early_clock_in_days)
