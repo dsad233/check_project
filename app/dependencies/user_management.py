bad65dc151b97a63764e3b9289e1c63a2feaed0d
MementoAI-Daejeong <marin@mement.ai>
2024-11-04 10:22:40 +0900

    FIX : dependencies 충돌 해결

--- app/dependencies/user_management.py
+++ app/dependencies/user_management.py
@@ -5,11 +5,13 @@ from app.core.database import get_db
 from app.cruds.user_management.base_crud import UserManagementRepository
 from app.cruds.user_management.contract_crud import UserManagementContractRepository
 from app.cruds.user_management.contract_info_crud import UserManagementContractInfoRepository
+from app.cruds.user_management.part_time_contract_crud import UserManagementPartTimeContractRepository
 from app.cruds.user_management.salary_contract_crud import UserManagementSalaryContractRepository
 from app.cruds.user_management.work_contract_crud import UserManagementWorkContractRepository
 from app.cruds.user_management.work_contract_history_crud import UserManagementContractHistoryRepository
 from app.service.user_management.contract_info_service import UserManagementContractInfoService
 from app.service.user_management.contract_service import UserManagementContractService
+from app.service.user_management.part_time_contract_service import UserManagementPartTimeContractService
 from app.service.user_management.salary_contract_service import UserManagementSalaryContractService
 from app.service.user_management.service import UserManagementService
 from app.service.user_management.work_contract_history_service import UserManagementContractHistoryService
@@ -17,34 +19,23 @@ from app.service.user_management.work_contract_service import UserManagementWork
 
 # UserManagement
 def get_user_management_repository(session: AsyncSession = Depends(get_db)):
-    return UserManagementRepository(
-        session=session
-    )
-
-def get_user_management_service(
-        user_management_repository: UserManagementRepository = Depends(get_user_management_repository)
-):
-    return UserManagementService(user_management_repository=user_management_repository)
+    return UserManagementRepository(session=session)
+def get_user_management_service(repository: UserManagementRepository = Depends(get_user_management_repository)):
+    return UserManagementService(repository=repository)
 
 
 # UserManagementWorkContractHistory
 def get_user_management_work_contract_history_repository(session: AsyncSession = Depends(get_db)):
-    return UserManagementContractHistoryRepository(
-        session=session
-    )
+    return UserManagementContractHistoryRepository(session=session)
 
 def get_user_management_work_contract_history_service(
         work_contract_history_repository: UserManagementContractHistoryRepository = Depends(get_user_management_work_contract_history_repository)):
-    return UserManagementContractHistoryService(
-        contract_history_repository=work_contract_history_repository
-    )
+    return UserManagementContractHistoryService(contract_history_repository=work_contract_history_repository)
 
 
 # UserManagementWorkContract
 def get_user_management_work_contract_repository(session: AsyncSession = Depends(get_db)):
-    return UserManagementSalaryContractRepository(
-        session=session
-    )
+    return UserManagementSalaryContractRepository(session=session)
 
 def get_user_management_work_contract_service(
         work_contract_repository: UserManagementWorkContractRepository = Depends(get_user_management_work_contract_repository),
@@ -57,22 +48,16 @@ def get_user_management_work_contract_service(
 
 # UserManagementSalaryContract
 def get_user_management_salary_contract_repository(session: AsyncSession = Depends(get_db)):
-    return UserManagementSalaryContractRepository(
-        session=session
-    )
+    return UserManagementSalaryContractRepository(session=session)
 
 def get_user_management_salary_contract_service(
         salary_contract_repository: UserManagementSalaryContractRepository = Depends(get_user_management_salary_contract_repository)):
-    return UserManagementSalaryContractService(
-        salary_contract_repository=salary_contract_repository
-    )
+    return UserManagementSalaryContractService(salary_contract_repository=salary_contract_repository)
 
 
 # UserManagementContract
 def get_user_management_contract_repository(session: AsyncSession = Depends(get_db)):
-    return UserManagementSalaryContractRepository(
-        session=session
-    )
+    return UserManagementSalaryContractRepository(session=session)
 
 def get_user_management_contract_service(
         user_management_service: UserManagementService = Depends(get_user_management_service),
@@ -91,9 +76,7 @@ def get_user_management_contract_service(
 
 # UserManagementContractInfo
 def get_user_management_contract_info_repository(session: AsyncSession = Depends(get_db)):
-    return UserManagementSalaryContractRepository(
-        session=session
-    )
+    return UserManagementSalaryContractRepository(session=session)
 
 def get_user_management_contract_info_service(
         user_management_service: UserManagementService = Depends(get_user_management_service),
@@ -104,3 +87,18 @@ def get_user_management_contract_info_service(
         contract_service=contract_service,
         contract_info_repository=contract_info_repository
     )
+
+
+# UserManagementPartTimeContract
+def get_user_management_part_time_contract_repository(session: AsyncSession = Depends(get_db)):
+    return UserManagementPartTimeContractRepository(session=session)
+
+def get_user_management_part_time_contract_service(
+        part_time_contract_repository: UserManagementPartTimeContractRepository = Depends(get_user_management_part_time_contract_repository)):
+    return UserManagementPartTimeContractService(part_time_contract_repository=part_time_contract_repository)
+
+
+
+# UserQuery
+def get_user_query_service():
+    return UserQueryService()
